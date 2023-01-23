9537c352a2f5b86182cae68950deae99d2dfa868
fanaimi <emiliano.fantasia@gmail.com>
2023-01-23 11:00:17 +0000

    movements and constraints

---
+++ blt-test/Assets/Scripts/CylinderController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BLTtest
+{
+    /**
+     * @obj     Cylinder
+     * @scene   BLTtest
+     * @desc    movement controller using arrow keys or WASD
+     */ 
+
+    public class CylinderController : MonoBehaviour
+    {
+        private float m_horizontalInput;
+        private float m_verticalInput;
+        
+        [SerializeField] private float m_speed = 20f;
+        [SerializeField] private float m_maxRange = 19.5f;
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovePlayer();
+        }
+
+
+        private void MovePlayer()
+        {   
+            // axis
+            m_horizontalInput = Input.GetAxis("Horizontal");
+            m_verticalInput = Input.GetAxis("Vertical");
+
+            // horizontal constraints
+            if (transform.position.x < -m_maxRange)
+            {
+                transform.position = new Vector3(
+                    -m_maxRange,
+                    transform.position.y,
+                    transform.position.z);
+            }
+
+            if (transform.position.x > m_maxRange)
+            {
+                transform.position = new Vector3(
+                    m_maxRange,
+                    transform.position.y,
+                    transform.position.z);
+            }
+
+            // vertical  constraints
+            if (transform.position.z > m_maxRange)
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    m_maxRange
+                );
+            }
+
+            if (transform.position.z < -m_maxRange)
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    -m_maxRange
+                );
+            }
+
+
+            // movements
+            transform.Translate(
+                new Vector3(
+                    m_horizontalInput * Time.deltaTime * m_speed,
+                    0f,
+                    m_verticalInput * Time.deltaTime * m_speed
+                )
+            );
+        }
+
+    }
+}
