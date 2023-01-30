b1fdf355dead10223904e642ede4ead1480b5fb3
fanaimi <emiliano.fantasia@gmail.com>
2023-01-30 16:07:38 +0000

    added playmode test

--- blt-test/Assets/AllTests/PlayModeTests/PlayModeTestScript.cs
+++ blt-test/Assets/AllTests/PlayModeTests/PlayModeTestScript.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using BLTtest;
 
 public class PlayModeTestScript
 {
     // A Test behaves as an ordinary method
-    [Test]
-    public void PlayModeTestScriptSimplePasses()
+    //[Test]
+    /*public void PlayModeTestScriptSimplePasses()
     {
         // Use the Assert class to test conditions
-    }
+    }*/
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator PlayModeTestScriptWithEnumeratorPasses()
+    public IEnumerator MoveNorth()
     {
+
+        var gameObject = new Object();
+        //var controller = gameObject.AddComponent<CylinderController>();
+
         // Use the Assert class to test conditions.
+
+        Assert.AreEqual(true, true);
+
         // Use yield to skip a frame.
         yield return null;
+
+        // Assert.AreEqual(true, true);
+
     }
 }
